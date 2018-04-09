6d9c2e2db53ac8a2e4e48e6b234d7480e2a82413
Chris <chomsey1@gmail.com>
2018-04-08 20:59:27 -0400

    User Card formatting

--- client/src/components/UserCard/UserCard.js
+++ client/src/components/UserCard/UserCard.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Grid } from 'semantic-ui-react';
 
 const UserCard = (props) => (
-  <Card>
-    <Card.Content>
-      <Image floated="left" size="mini" src={props.userAvatar} circular />
-      <Card.Header>{props.name}</Card.Header>
-    </Card.Content>
-    <Image src={props.userImage} />
-    <Card.Content>
-      <Card.Description>{props.description}</Card.Description>
-    </Card.Content>
-    <Card.Content extra>{props.extra}</Card.Content>
-  </Card>
+    <Card fluid>
+      <Card.Content>
+        <Image floated="left" size="mini" src={props.userAvatar} circular />
+        <Card.Header>{props.name}</Card.Header>
+      </Card.Content>
+      <Image src={props.userImage} />
+      <Card.Content>
+        <Card.Description>{props.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>{props.extra}</Card.Content>
+    </Card>
 );
 
 export default UserCard;
