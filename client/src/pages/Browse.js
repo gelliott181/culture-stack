c0ff60492fa37497908c4f454c24d033fb27346a
Chris <chomsey1@gmail.com>
2018-04-10 21:46:03 -0400

    Best browse

--- client/src/pages/Browse.js
+++ client/src/pages/Browse.js
@@ -64,49 +64,6 @@ export default class Browse extends Component  {
                     
                 </Grid.Column>
                 
-                
-                
-                {/* <Grid.Row>
-                    <Grid.Column mobile={16} only="mobile">
-                        <Header as="h2">Tags</Header>
-                        <div className="secondary-font">
-                            <span>#nature </span>
-                            <span>#landscape </span>
-                            <span>#fashion </span>
-                            <span>#nature </span>
-                            <span>#landscape </span>
-                            <span>#fashion </span>
-                            <span>#fashion </span>
-                            <span>#fashion </span>
-                            <span>#fashion </span>
-                        </div>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} tablet={12} computer={12}><Divider horizontal>#{this.state.tag}</Divider></Grid.Column>
-                    <Grid.Column only="table computer" tablet={4} computer={4}><Divider horizontal>Tags</Divider></Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column mobile={16} tablet={4} computer={4}>
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />                
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                </Grid.Column>
-                <Grid.Column mobile={16} tablet={4} computer={4}>
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />                
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                </Grid.Column>
-                <Grid.Column mobile={16} tablet={4} computer={4}>
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />                
-                    <PostCard name="Test" userImage="/img/default_placeholder.png" userAvatar="img/default_avatar.jpg" description="description" extra="Extra" />
-                </Grid.Column>
-                    <Grid.Column only="table computer"tablet={4} computer={4}>
-                    <button name="tag" onClick={this.handleTagChange}>TEST</button>
-                </Grid.Column>
-                </Grid.Row> */}
             </Grid>
         </Container>
         )
