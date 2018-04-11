22e5c3e42231db5b5c6cb07d1f5df7016fe1366f
Chris <chomsey1@gmail.com>
2018-04-10 20:03:41 -0400

    created PostCards

---
+++ client/src/components/PostCard/PostCard.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Card, Image, Grid } from 'semantic-ui-react';
+import {Link} from "react-router-dom";
+
+
+export default class PostCard extends React.Component {
+  
+  render(props) {
+
+    const imageStyle = {
+      backgroundImage: "url("+this.props.postImage+")",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "240px"
+    }
+
+    // const wrapStyle = {
+    //   backgroundColor: "black",
+    //   display: "block",
+    //   width: "100%",
+    //   position: "relative",
+    //   height: "0",
+    //   padding: "56.25% 0 0 0",
+    //   overflow: "hidden"
+    // }
+
+    // const imgStyle = {
+    //   position: "absolute",
+    //   display: "block",
+    //   maxWidth: "100%",
+    //   maxHeight: "100%",
+    //   left: "0",
+    //   right: "0",
+    //   top: "0",
+    //   bottom: "0",
+    //   margin: "auto"
+    // }
+
+    return(
+      <Card fluid>
+        <Card.Content>
+          <Image floated="left" size="mini" src={this.props.userAvatar} circular />
+          <Card.Header>{this.props.name}</Card.Header>
+
+          {/* When ready, this should link to {`/user/${this.props.author._id}`} */}
+          <Card.Meta as={Link} to={`/user`}>{this.props.author}</Card.Meta>
+        </Card.Content>
+        {/* <div style={ wrapStyle }>
+          <img style={imgStyle} src={this.props.postImage} />
+        </div> */}
+
+        <div style={ imageStyle }></div>
+        <Card.Content>
+          <Card.Description>{this.props.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    )
+  };
+}
